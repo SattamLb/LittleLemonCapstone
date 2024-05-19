85b8d03fa9f3e5fd1152dbccc871b41e97bfc135
SattamLb <stam01245@gmail.com>
2024-05-19 16:44:34 +0300

    Initial Commit

---
+++ LittleLemonCapstone/LittleLemonCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonCapstoneApp.swift
+//  LittleLemonCapstone
+//
+//  Created by Sattam Bandar Albogami on 11/11/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
